28424a3800bc27c3ad6cc2888dec3c5d9b022098
Zosiamil <134380810+Zosiamil@users.noreply.github.com>
2023-06-10 16:22:18 +0200

    Update program1.py

--- program1.py
+++ program1.py
@@ -2,3 +2,4 @@ for i in range(1, 101):
     Fizz_Buzz = "FizzBuzz" if i % 15 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else i
     print(Fizz_Buzz)
 #to jest 1 edycja
+#to jest 2 edycja
