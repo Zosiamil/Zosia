104ff49db7989c7eb916bba7683ec9ee17d0a6e6
Zosiamil <134380810+Zosiamil@users.noreply.github.com>
2023-06-10 16:20:29 +0200

    Create program2.py

---
+++ program2.py
@@ -0,0 +1,7 @@
+def srednia(lista):
+    srednia = sum(lista) / len(lista)
+    return srednia
+
+
+moja_lista = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 8, 9, 10]
+print(srednia(moja_lista))
